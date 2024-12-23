1abe1d4b67cacce647f159be147de73afbe4dcc2
Md Sabbir Ahmed Khan <rey.sabbir@gmail.com>
2024-12-23 21:47:29 +0800

    Fixed build error

--- features/home/src/androidTest/java/xyz/androidrey/weatherapp/home/ExampleInstrumentedTest.kt
+++ features/home/src/androidTest/java/xyz/androidrey/weatherapp/home/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("xyz.androidrey.weatherapp.dashboard.test", appContext.packageName)
+        assertEquals("xyz.androidrey.weatherapp.home.test", appContext.packageName)
     }
 }
